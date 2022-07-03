6935329e637ad6c1a2a488fe334b9e2985ea66cd
Alina Dudzic <alina@dudzic.pl>
2022-07-03 22:46:22 +0200

    new

--- valid.php
+++ valid.php
@@ -1,4 +1,6 @@
 <?php
+const SIZE_FACTOR = 112000;
+
 $height = $_POST['height'] ?? 0;
 
 $width = $_POST['width'] ?? 0;
@@ -7,9 +9,9 @@ $length = $_POST['length'] ?? 0;
 
 if (strlen($width) > 1 && strlen($height) > 1 && strlen($length) > 1) {
     $result = (int) $width * (int) $height * (int) $length;
-    if (($result / 112000) >= 1) {
+    if (($result / SIZE_FACTOR) >= 1) {
         echo 'Twoja klatka pomieści ';
-        $cage = round($result / 112000);
+        $cage = round($result / SIZE_FACTOR);
         echo $cage;
         if ($cage > 3) {
             echo ' szynszyli';
@@ -17,7 +19,7 @@ if (strlen($width) > 1 && strlen($height) > 1 && strlen($length) > 1) {
             echo ' szynszyle';
         }
         echo ' i powinna mieć nie mniej niż ';
-        $shelf = 1 + (round($result / 112000));
+        $shelf = 1 + (round($result / SIZE_FACTOR));
         echo $shelf;
         if ($shelf > 4) {
             echo ' półek';
