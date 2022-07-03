5612e605f3604563b57627ef078387d5ab06e054
Alina Dudzic <alina@dudzic.pl>
2022-07-03 21:44:00 +0200

    ort fix

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
        <div class="flex"> <form method="POST" action="valid.php">
             <div><span class="input_description">wysokość:</span></div> <div> <input type="number" name="height" required><br></div>
             <div><span class="input_description">szerokość:</span></div><div> <input type="number" name="width" required><br></div>
-            <div><span class="input_description">długość:</span></div><div> <input type="number" name="lenght" required><br></div>
+            <div><span class="input_description">długość:</span></div><div> <input type="number" name="length" required><br></div>
             <div><input class="button" type="submit"></div>
         </form></div></div><div?></div>
     </body>
